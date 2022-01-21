c55aa2a38743b7b62e10587068c2d6204ab66b2e
Octavio Gonzalez <octavio_ga@hotmail.com>
2022-01-20 21:39:30 -0600

    Workshop 2 Finish

--- README.md
+++ README.md
@@ -8,3 +8,15 @@
 - Second step: Implement the formules on javascript
 - Third step: Create the functions
 - Fourth step: Integrate javascript with html
+
+## Workshop #2: Percentages
+
+- First step: Define the formules
+- Second step: Implement the formules on javascript
+- Third step: Create the functions
+- Fourth step: Integrate javascript with html
+
+
+
+
+This is my firt time working with html, css and javascript :D
---
+++ percentages.js
@@ -0,0 +1,25 @@
+function getDiscountPrice(price, percentage){
+    const discount=percentage*price/100;
+    return price-discount;
+}
+
+
+function onInputPriceDiscount(){
+    const inputPrice = document.getElementById("inputPrice");
+    const inputDiscount = document.getElementById("inputDiscount");
+
+    if (inputDiscount.value != "" && inputPrice.value != ""){
+        
+        var originalPrice = parseInt(inputPrice.value);
+        var discount = parseInt(inputDiscount.value);
+        var newPrice = getDiscountPrice(originalPrice,discount);
+
+        document.getElementById("finalPrice").innerText = "$"+newPrice.toFixed(2) ;
+    } else {
+
+        document.getElementById("finalPrice").innerText = "$0.00";
+    }
+    
+}
+
+
